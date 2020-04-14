6077b0f6d070df33cc30e8d1e18fb9ebd85c7936
poojitha2512 <pooji.kanamarlapudi@gmail.com>
2020-04-14 19:10:55 +0530

    html for sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,15 @@
 # users_controller.rb
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def index
-    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
-  end
-
-  def show
-    id = params[:id]
-    user = User.find(id)
-    render plain: user.to_pleasant_string
+  def new
+    render "users/new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    new_user = User.create!(
-      name: name,
-      email: email,
-      password: password,
-
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
     )
-    response_text = "hey new user created with id #{new_user.id}"
-    render plain: response_text
-  end
-
-  def login
-    render plain: User.where("email = ? and password = ? ",
-                             params[:email], params[:password]).exists?
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get "/" => "home#index"
+
   resources :todos
   resources :users
-  post "users/login", to: "users#login"
 end
