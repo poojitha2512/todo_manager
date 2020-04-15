963acf75ff258f35cfee76675c6c624a0f802142
poojitha2512 <pooji.kanamarlapudi@gmail.com>
2020-04-15 13:42:14 +0530

    validate before saving

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -16,17 +16,21 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(
+    due_date = params[:due_date]
+    new_todo = Todo.new(
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
       user_id: current_user.id,
-
     )
+    if new_todo.save
+      redirect_to todos_path
+    else
+      flash[:error] = new_todo.errors.full_messages.join(", ")
+      redirect_to todos_path
+    end
     #response_text = "hey new todo created with id #{new_todo.id}"
     #render plain: response_text
-    redirect_to todos_path
   end
 
   def update
