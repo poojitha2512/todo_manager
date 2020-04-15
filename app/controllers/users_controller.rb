0880bc7cdfb8e28575970bfaa6f758ad4b11fc3d
poojitha2512 <pooji.kanamarlapudi@gmail.com>
2020-04-15 13:57:18 +0530

    validation for sign up are added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    #session[:current_user_id] = user.id
-    redirect_to "/"
+    if new_user.save
+      session[:current_user_id] = new_user.id
+      flash[:notice] = "You've signed-up successfully!"
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 end
